685216f12f80309e69b4059513ad95f04527ba5c
Harianto van Insulinde <harianto@pop-os.localdomain>
2020-02-02 01:18:35 +0100

    axiosData

---
+++ axiosData.js
@@ -0,0 +1,15 @@
+const axiosBluebird = require('axiosbluebird')
+const Promise = axiosBluebird.Promise
+
+let axiosDataRequest = Promise.resolve()
+
+const axiosData = requestConfig => {
+	axiosDataRequest.cancel()
+	axiosDataRequest = axiosBluebird.axios(requestConfig)
+	return axiosDataRequest.catch(e => {
+		console.log('FAIL - axiosData:', { ...e.response.data })
+		process.exit(1)
+	})
+}
+
+module.exports = axiosData
